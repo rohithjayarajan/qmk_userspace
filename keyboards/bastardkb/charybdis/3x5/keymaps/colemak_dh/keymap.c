4e1ed425dee6eb7720e5ef2570591e50e7693c4d
rohithjayarajan <rohith.jayarajan@gmail.com>
2025-03-31 01:34:34 -0700

    revert arrow keys for 3x5

--- keyboards/bastardkb/charybdis/3x5/keymaps/colemak_dh/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/colemak_dh/keymap.c
@@ -124,8 +124,8 @@ static uint16_t auto_pointer_layer_timer = 0;
  * base layer to avoid having to layer change mid edit and to enable auto-repeat.
  */
 #define LAYOUT_LAYER_NAVIGATION                                                               \
-    _______________DEAD_HALF_ROW_______________, XXXXXXX, XXXXXXX, KC_UP, XXXXXXX, XXXXXXX, \
-    ______________HOME_ROW_GACS_L______________, KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, \
+    _______________DEAD_HALF_ROW_______________, _______________DEAD_HALF_ROW_______________, \
+    ______________HOME_ROW_GACS_L______________, KC_CAPS, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, \
     _______________DEAD_HALF_ROW_______________,  KC_INS, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, \
                       XXXXXXX, _______, XXXXXXX,  KC_ENT, KC_BSPC
 
